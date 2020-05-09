be6bcfe8e3c6d42d2d6db7f5194a0fedc8dbd355
ClementLong <clmt.long@gmail.com>
2020-05-09 15:14:11 +0200

    init header & footer

--- components/index.js
+++ components/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 
 // Root
+import Header from './Header'
+import Footer from './Footer'
 import Logo from './Logo'
 
+Vue.component('Header', Header)
+Vue.component('Footer', Footer)
 Vue.component('Logo', Logo)
 
 // // Cards
