570b800ab18b5a1823c056369c542fb06886b0fb
ClementLong <clmt.long@gmail.com>
2020-05-09 14:17:35 +0200

    init nuxt

---
+++ components/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+
+// Root
+import Logo from './Logo'
+
+Vue.component('Logo', Logo)
+
+// // Cards
+// import cards from './cards'
+
+// Vue.component('Card', cards.Card)
+// Vue.component('HiddenCard', cards.HiddenCard)
+// Vue.component('Rank', cards.Rank)
+// Vue.component('Suit', cards.Suit)
+
+// // Lobby
+// import lobby from './lobby'
+
+// Vue.component('List', lobby.List)
+// Vue.component('Team', lobby.Team)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
-/*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
 module.exports = {
-  theme: {},
-  variants: {},
-  plugins: []
+	theme: {
+		fontFamily: {
+			'open-sans': ['Open Sans', 'sans-serif']
+		},
+		extend: {
+			colors: {
+				black: '#0D0D0C',
+				white: '#DDDDDD',
+				first: '#C23113',
+				second: '#C85E3E',
+				third: '#C78B72',
+				fourth: '#CBB3A3',
+				fifth: '#DCD0C4',
+			},
+			spacing: {
+				'75vh': '75vh',
+				'50vh': '50vh',
+				'25vh': '25vh',
+				'20vh': '20vh',
+				'100px': '100px'
+			}
+		 }
+	},
+	variants: {
+		borderWidth: ['odd'],
+	},
+	plugins: []
 }
