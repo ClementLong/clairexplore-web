ef055b40443484b8f422cb0cb0a104387be7357d
ClementLong <clmt.long@gmail.com>
2020-06-04 21:34:56 +0200

    init article factory

--- pages/_article.vue
+++ pages/_article.vue
@@ -1,13 +1,30 @@
 <template>
 	<div>
 		<ArticleHeader
-			title="15 EXPÉRIENCES À NE PAS MANQUER LORS DE VOTRE VOYAGE À DUBAÏ"
-			date="2020-07-01"
-			location="Emirats Arabes Unis"
-			cover="/images/test.jpg"
+			:title="article.title"
+			:date="article.date"
+			:location="article.country"
+			:cover="article.cover"
 		/>
 
-		<Title text="1. Monter en haut de la Tour Burj Khalifa et profiter de la vue sur tout Dubaï" />
+		<div v-for="(component, index) in article.content" :key="index">
+			<Single
+				v-if="component.__component == 'article.single'"
+				:size="component.size"
+				:image="component.image"
+				:alt="component.image.alternativeText"
+				:title="component.image.caption"
+			/>
+
+			<Title
+				v-if="component.__component == 'article.title'"
+				:size="component.size"
+				:text="component.title"
+			/>
+
+		</div>
+
+		<!-- <Title text="1. Monter en haut de la Tour Burj Khalifa et profiter de la vue sur tout Dubaï" />
 		<Paragraph text="La crique est le bras de mer qui séparent deux quartiers historiques de Dubaï : Bur Dubaï et Deira. Pour se rendre d’un quartier à l’autre, je vous conseille de monter à bord d’un abra. C’est une embarcation traditionnelle en bois accessible depuis l’une des stations situées le long de la crique." />
 		<Subtitle text="Monter en haut de la Tour Burj Khalifa et profiter de la vue sur tout Dubaï with long text" />
 		<Paragraph text="La crique est le bras de mer qui séparent deux quartiers historiques de Dubaï : Bur Dubaï et Deira. Pour se rendre d’un quartier à l’autre, je vous conseille de monter à bord d’un abra. C’est une embarcation traditionnelle en bois accessible depuis l’une des stations situées le long de la crique." />
@@ -19,10 +36,9 @@
 
 		<Divider />
 
-		<Single image="/images/test.jpg" alt="nom de l'image" />
-		<Single image="/images/test2.jpg" alt="nom de l'image" title="Titre de l'image" />
-
-		<BigPicture image="/images/test2.jpg" alt="nom de l'image" title="Titre de l'image" />
+		<Single size="small" image="/images/test.jpg" alt="nom de l'image" />
+		<Single size="big" image="/images/test2.jpg" alt="nom de l'image" title="Titre de l'image" />
+		<Single size="cover" image="/images/test2.jpg" alt="nom de l'image" title="Titre de l'image" />
 
 		<Duo
 			left="/images/test.jpg" left-alt="nom de l'image"
@@ -61,9 +77,6 @@
 			text="La crique est le bras de mer qui séparent deux quartiers historiques de Dubaï : Bur Dubaï et Deira. Pour se rendre d’un quartier à l’autre, je vous conseille de monter à bord d’un abra. C’est une embarcation traditionnelle en bois accessible depuis l’une des stations situées le long de la crique."
 		/>
 
-
-		<Cover image="/images/test.jpg" alt="nom de l'image" title="test" />
-
 		<Scotch image="/images/test.jpg" />
 
 		<PolaSingle image="/images/test.jpg" alt="nom de l'image" title="test" />
@@ -96,18 +109,18 @@
 		<Map url="https://www.google.com/maps/d/embed?mid=18WmJKsjMiMmpWQjosrVfnhD9owQQPK5R" />
 		<Slider
 			:images="[{ url: '/images/test2.jpg'},{ url:'/images/test.jpg'}]"
-		/>
+		/> -->
 
 	</div>
 </template>
 
 <script lang="ts">
-// import axios from 'axios'
+import axios from 'axios'
 
 export default {
-	// async asyncData({ params }: any) {
-	// 	const { data } = await axios.get(`https://my-api/posts/${params.article}`)
-	// 	return { content: data }
-	// }
+	async asyncData({ params }: any) {
+		const article = await axios.get(`http://localhost:1337/articles?slug=${params.article}`)
+		return { article: article.data[0] }
+	}
 }
 </script>
