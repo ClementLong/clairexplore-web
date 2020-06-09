83b52001e35c21b88960bb517dee1d0a5b30d63b
ClementLong <clmt.long@gmail.com>
2020-06-09 21:57:19 +0200

    fix homepage

--- components/pages/Presentation.vue
+++ components/pages/Presentation.vue
@@ -1,11 +1,11 @@
 <template>
 	<div>
-		<img class="w-full" :src="'http://localhost:1337' + cover" alt="Blog de voyage">
+		<img class="w-full" :src="cover" alt="Blog de voyage">
 		<div class="container m-auto flex items-center pt-5 pb-12">
 			<div class="text-center text-lightblack w-1/2 px-8" v-html="introduction">
 			</div>
 			<div class="w-1/2 px-8">
-				<img :src="'http://localhost:1337' + introductionImage" alt="Photo de voyage">
+				<img :src="introductionImage" alt="Photo de voyage">
 			</div>
 		</div>
 	</div>
