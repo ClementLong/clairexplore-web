60dc2cebabc7e057f26686d047e56d6f66b3cb22
ClementLong <clmt.long@gmail.com>
2020-06-10 14:50:07 +0200

    test

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
+const wwwhisper = require('connect-wwwhisper')
+
+app.use(wwwhisper())
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
