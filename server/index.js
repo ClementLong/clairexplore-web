6158f213435a713a25f19520476014a0e3b9b7fb
ClementLong <clmt.long@gmail.com>
2020-06-10 15:03:08 +0200

    add password for staging

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
+const basicAuth = require('express-basic-auth')
+
+if(process.env.STAGING) {
+	app.use(basicAuth({
+		users: { 'admin': 'Evreux' },
+		challenge: true
+	}))
+}
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
