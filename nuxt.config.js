f7411e061b04d7aeb827f71669a675f43d4d9105
ClementLong <clmt.long@gmail.com>
2020-06-16 19:14:26 +0200

    add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,7 @@ module.exports = {
 		],
 		link: [
 			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			{ rel: 'icon', type: 'image/png', sizes:"32x32", href: '/favicon-32x32.png' },
 			{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,900;1,100;1,200;1,400;1,600;1,700&display=swap' },
 		]
 	},
