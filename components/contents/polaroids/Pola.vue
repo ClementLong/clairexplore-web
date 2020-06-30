7fad07de1f235473fc3f34d4005d90836d688397
ClementLong <clmt.long@gmail.com>
2020-06-30 16:28:17 +0200

    init pola

--- components/contents/polaroids/Pola.vue
+++ components/contents/polaroids/Pola.vue
@@ -1,19 +1,15 @@
 <template>
-	<div
-		:class="[
-			'm-auto pb-5 flex',
-			images.length === 1 ? 'small-container' : 'container',
-		]"
-	>
+	<div class="m-auto pb-5 flex" :class="containerStyle">
 		<div
 			v-for="(image, index) in images"
 			:key="index"
-			class="bg-darkwhite p-6 pb-16 shadow relative flex-1"
+			class="bg-darkwhite lg:p-6 p-2 lg:pb-16 pb-10 shadow relative flex-1"
+			:style="polaStyle(index)"
 		>
-			<Photo :image="image" />
+			<img v-if="image.formats.large" :src="MEDIA_URL + image.formats.large.url" :alt="image.alternativeText" class="h-full w-full object-cover">
 			<h5
 				v-if="image.caption"
-				class="absolute left-0 text-lightblack font-heading font-bold pt-4 text-xl text-center w-full"
+				class="absolute left-0 text-lightblack font-heading font-light italic lg:pt-5 px-1 pt-1 lg:text-lg text-xs text-center w-full"
 			>
 				{{ image.caption }}
 			</h5>
@@ -21,13 +17,48 @@
 	</div>
 </template>
 
-<script lang="ts">
+<script>
 export default {
 	props: {
 		images: {
 			type: Array,
 			required: true
 		}
+	},
+	data() {
+		return {
+			MEDIA_URL: process.env.MEDIA_URL,
+		}
+	},
+	computed: {
+		containerStyle() {
+			if(this.images.length === 1) {
+				return 'small-container'
+			} else {
+				return 'container'
+			}
+		}
+	},
+	methods: {
+		polaStyle(index) {
+			if(this.images.length > 2) {
+				if(index === 0) {
+					return 'transform: rotate(-5deg) translate(20px, 25px);'
+				} else if (index === 1) {
+					return 'transform: rotate(0deg); z-index: 2;'
+				} else {
+					return 'transform: rotate(5deg) translate(-25px, 20px);'
+				}
+			} else if(this.images.length > 1) {
+				if(index === 0) {
+					return 'transform: rotate(-4deg) translate(15px, 10px); height:70vh;'
+				} else {
+					return 'transform: rotate(2deg) translate(-15px, 10px); height:70vh;'
+				}
+			} else {
+				return ''
+			}
+		},
 	}
 }
 </script>
