10f3e4f6e7b0f8bca96c9fd304f26b5d2f0b7da7
ClementLong <clmt.long@gmail.com>
2020-07-20 18:02:37 +0200

    remove pwd

--- server/index.js
+++ server/index.js
@@ -2,16 +2,10 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
-const basicAuth = require('express-basic-auth')
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = process.env.NODE_ENV !== 'production'
-
-app.use(basicAuth({
-	users: { 'admin': 'admin' },
-	challenge: true,
-}))
 
 async function start () {
   // Init Nuxt.js
