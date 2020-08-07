59b09748e1bc63a713e0c6401863a16fb325dcc6
ClementLong <clmt.long@gmail.com>
2020-08-07 17:19:51 +0200

    fix mixed and duo for no bug with empty data

--- components/contents/images/Duo.vue
+++ components/contents/images/Duo.vue
@@ -1,12 +1,12 @@
 <template>
 	<div class="container m-auto pb-5 flex items-center">
 		<div v-if="left" class="pr-1 lg:pr-5 flex-1">
-			<Photo :image="left" />
-			<h5 v-if="left.caption" class="text-lightblack font-heading font-bold pt-2 text-center">{{ left.caption }}</h5>
+			<Photo v-if="left" :image="left" />
+			<h5 v-if="left && left.caption" class="text-lightblack font-heading font-bold pt-2 text-center">{{ left.caption }}</h5>
 		</div>
 		<div v-if="right" class="pl-1 lg:pl-5 flex-1">
-			<Photo :image="right" />
-			<h5 v-if="right.caption" class="text-lightblack font-heading font-bold pt-2 text-center">{{ right.caption }}</h5>
+			<Photo v-if="right" :image="right" />
+			<h5 v-if="right && right.caption" class="text-lightblack font-heading font-bold pt-2 text-center">{{ right.caption }}</h5>
 		</div>
 	</div>
 </template>
