d41becf5e6770354dae7a9247687db1799e33adb
Diele <clmt.long@gmail.com>
2022-12-27 16:54:36 +0100

    fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<Header ref="header" />
-		<div class="md:pt-50px min-h-screen" :class="{ 'fixed': $refs.header.isOpen }">
+		<div class="md:pt-50px min-h-screen" :class="{ 'fixed': $refs.header && $refs.header.isOpen }">
 			<nuxt />
 		</div>
 		<Footer />
